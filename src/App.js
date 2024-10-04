cc346f1bf21bb7719a222e10f1601c7d32be6378
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-04 14:32:35 +0700

    feat: create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
-<h1>Calculate App</h1>
+      <h1>Calculate App</h1>
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      <div className="calculator-display">
+        <h1>0</h1>
+      </div>
+      <div className="calculator-btn">
+        <button className="operator">+</button>
+        <button className="operator">–</button>
+        <button className="operator">×</button>
+        <button className="operator">÷</button>
+        {/* Number */}
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="clear-btn">C</button>
+        <button className="equal">=</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
